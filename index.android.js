709e6cbcdf592e9adf61bd8983297f75f8ae9b18
Brian Tatum <brianttatum@gmail.com>
2017-05-14 20:47:14 -0500

    Add Layout commonent to ios and removed styles from both.

--- index.android.js
+++ index.android.js
@@ -5,46 +5,16 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 
 import LayOut from './myapp/layOut'
 
 export default class FirstProj extends Component {
   render() {
     return (
-        <LayOut />
+      <LayOut />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  mine: {
-    fontSize: 40,
-    textAlign: 'center',
-    color: '#006600',
-    margin: 25
-  }
-});
 
 AppRegistry.registerComponent('FirstProj', () => FirstProj);
